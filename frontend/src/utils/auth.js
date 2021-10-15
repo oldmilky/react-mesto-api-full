b03f75fc5be89bb1cd45ba9df310b323210eac52
oldmilky <oldmilky@yandex.kz>
2021-10-16 01:11:34 +0300

    test

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://backend.oldmilky.nomoredomains.club';
+export const BASE_URL = 'https://backend.oldmilky.nomoredomains.club';
 
 const checkRequestResult = (res) => {
   if (res.ok) {
